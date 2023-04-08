8a98604d1b85c505d13d927eeed0d3c94c1c6e7d
Dmitry <62187726+Solowayer@users.noreply.github.com>
2023-04-08 22:14:31 +0300

    Creating Prisma schema

---
+++ src/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+})
+export class AppModule {}
