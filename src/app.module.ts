063c153b95ef628788a4ec0cb8233d69dbb119b4
Dmitry <62187726+Solowayer@users.noreply.github.com>
2023-04-08 22:17:25 +0300

    Prisma settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
