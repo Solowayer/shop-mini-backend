2bb04e9438c1cc962f9fd1d3edb2b90abec2748d
Dmitry <62187726+Solowayer@users.noreply.github.com>
2023-06-17 16:34:45 +0300

    First upload file

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,34 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UploadService } from './upload.service';
-import { CreateUploadDto } from './dto/create-upload.dto';
-import { UpdateUploadDto } from './dto/update-upload.dto';
+import { Controller, Post, UseInterceptors, UploadedFile } from '@nestjs/common'
+import { FileInterceptor } from '@nestjs/platform-express'
+import { diskStorage } from 'multer'
 
 @Controller('upload')
 export class UploadController {
-  constructor(private readonly uploadService: UploadService) {}
+	// constructor(private readonly uploadService: UploadService) {}
 
-  @Post()
-  create(@Body() createUploadDto: CreateUploadDto) {
-    return this.uploadService.create(createUploadDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.uploadService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.uploadService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUploadDto: UpdateUploadDto) {
-    return this.uploadService.update(+id, updateUploadDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.uploadService.remove(+id);
-  }
+	@Post()
+	@UseInterceptors(
+		FileInterceptor('file', {
+			storage: diskStorage({
+				destination: './uploads',
+				filename: (req, file, callback) => {
+					const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+					const filename = `${file.fieldname}-${uniqueSuffix}`
+					callback(null, filename)
+				}
+			})
+		})
+	)
+	uploadFile(@UploadedFile() file: Express.Multer.File) {
+		console.log(file)
+		return { message: 'Файл завантажено успішно' }
+	}
 }
--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { UploadService } from './upload.service';
-import { UploadController } from './upload.controller';
+import { Module } from '@nestjs/common'
+import { UploadService } from './upload.service'
+import { UploadController } from './upload.controller'
+import { MulterModule } from '@nestjs/platform-express'
 
 @Module({
-  controllers: [UploadController],
-  providers: [UploadService]
+	imports: [MulterModule.register({ dest: './uploads' })],
+	controllers: [UploadController],
+	providers: [UploadService]
 })
 export class UploadModule {}
