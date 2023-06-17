a9927154cfa1734eea99febb3feb64877f96edfc
Dmitry <62187726+Solowayer@users.noreply.github.com>
2023-06-17 19:01:48 +0300

    Upload images array

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,16 +1,15 @@
-import { Controller, Post, UseInterceptors, UploadedFile } from '@nestjs/common'
-import { FileInterceptor } from '@nestjs/platform-express'
+/* eslint-disable no-console */
+import { Controller, Post, UseInterceptors, UploadedFile, UploadedFiles, BadRequestException } from '@nestjs/common'
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express'
 import { diskStorage } from 'multer'
 
 @Controller('upload')
 export class UploadController {
-	// constructor(private readonly uploadService: UploadService) {}
-
-	@Post()
+	@Post('file')
 	@UseInterceptors(
 		FileInterceptor('file', {
 			storage: diskStorage({
-				destination: './uploads',
+				destination: './uploads/files',
 				filename: (req, file, callback) => {
 					const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
 					const filename = `${file.fieldname}-${uniqueSuffix}`
@@ -22,5 +21,33 @@ export class UploadController {
 	uploadFile(@UploadedFile() file: Express.Multer.File) {
 		console.log(file)
 		return { message: 'Файл завантажено успішно' }
+	}
+
+	@Post('image')
+	@UseInterceptors(
+		FilesInterceptor('images', 10, {
+			storage: diskStorage({
+				destination: './uploads/images',
+				filename: (req, file, callback) => {
+					const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+					const filename = `${file.fieldname}-${uniqueSuffix}`
+					callback(null, filename)
+				}
+			}),
+			fileFilter: (req, file, callback) => {
+				if (file.mimetype.match(/\/(jpeg|png|gif)$/)) {
+					callback(null, true)
+				} else {
+					callback(new BadRequestException('Invalid file type'), false)
+				}
+			},
+			limits: {
+				fileSize: 1000 * 1000 * 2 // 2MB
+			}
+		})
+	)
+	uploadImages(@UploadedFiles() files: Express.Multer.File[]) {
+		console.log(files)
+		return { message: 'Завантажено успішно' }
 	}
 }
